efffea4a4bcd9ec70912ee79bd4797dd2c883f8d
Paulo Alexandre <pacalexandre@gmail.com>
2022-07-26 12:25:19 +0000

    funcionando rapi rest + pagina html

--- app/core/views.py
+++ app/core/views.py
@@ -1,23 +1,34 @@
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin,RetrieveModelMixin,CreateModelMixin,UpdateModelMixin
 
-class ApiViewSet(ListModelMixin, GenericViewSet):
 
+class ApiViewSet(ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, GenericViewSet):
     def list(self, request):
         return Response(data={"text":"Hellou Mundo"}, status=status.HTTP_200_OK)
-    
+
     def retrieve(self, request, pk=None):
-        return Response(data={"text":pk}, status=status.HTTP_200_OK)
+        body = {'id':pk, 'busca': True}
+        return Response(data=body, status=status.HTTP_200_OK)
 
     def create(self,request):
-        return Response(data={"text":"Hellou Mundo"}, status=status.HTTP_201_CREATED)
-    
-    def delete(self, request):
-        return Response(data={}, status=status.HTTP_200_OK)
-    
-    def path(self, request, pk=None):
-        return Response(data={}, status=status.HTTP_200_OK)
+        body = request.data
+        return Response(data=body, status=status.HTTP_201_CREATED)
 
+    def delete(self, request, pk=None):
+        if pk:
+            body = request.data
+            body = {'delete':True}
+            status = status.HTTP_200_OK
+        else:
+            body = {'delete':False}
+            status = status.HTTP_400
+        return Response(data=body, status=status)
 
+    def update(self, request, pk=None):
+        body = request.data
+        body.update({'atualizado':True})
+        body.update({'id': pk})
+        return Response(data=body, status=status.HTTP_200_OK)
