6662d43367f06e0d90554d908647af609f457680
Paulo Alexandre <pacalexandre@gmail.com>
2022-07-26 13:03:02 +0000

    end points atualizados

--- app/core/views.py
+++ app/core/views.py
@@ -1,4 +1,4 @@
-
+"""API rest"""
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -6,29 +6,44 @@ from rest_framework.mixins import ListModelMixin,RetrieveModelMixin,CreateModelM
 
 
 class ApiViewSet(ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, GenericViewSet):
+
     def list(self, request):
-        return Response(data={"text":"Hellou Mundo"}, status=status.HTTP_200_OK)
+        lista = []
+        lista.append({"text":"Hellou Mundo"})
+        return Response(data=lista, status=status.HTTP_200_OK)
+
 
     def retrieve(self, request, pk=None):
-        body = {'id':pk, 'busca': True}
+        dados = request.data
+        body = {'id':pk, 'busca': True,'dados': dados}
         return Response(data=body, status=status.HTTP_200_OK)
+
 
     def create(self,request):
         body = request.data
         return Response(data=body, status=status.HTTP_201_CREATED)
 
+
     def delete(self, request, pk=None):
         if pk:
             body = request.data
             body = {'delete':True}
-            status = status.HTTP_200_OK
+            status_code = status.HTTP_200_OK
         else:
             body = {'delete':False}
-            status = status.HTTP_400
-        return Response(data=body, status=status)
+            status_code = status.HTTP_400_BAD_REQUEST
+        return Response(data=body, status=status_code)
+
 
     def update(self, request, pk=None):
         body = request.data
         body.update({'atualizado':True})
+        body.update({'id': pk})
+        return Response(data=body, status=status.HTTP_200_OK)
+
+
+    def partial_update(self, request, pk=None):
+        body = request.data
+        body.update({'parcial':True})
         body.update({'id': pk})
         return Response(data=body, status=status.HTTP_200_OK)
